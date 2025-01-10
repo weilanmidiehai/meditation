75cdd1e19c245dce758eb48cea49da0369297a5c
Lily <103798802+xuel59565@users.noreply.github.com>
2025-01-10 11:08:44 +0800

    頭像样式

--- lib/pages/my/my_page.dart
+++ lib/pages/my/my_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyPage extends StatefulWidget {
   const MyPage({super.key});
@@ -12,7 +13,27 @@ class _MyPageState extends State<MyPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('My Page')),
-      body: const Center(child: Text('My Page')),
+      body: Container(
+          padding: EdgeInsets.all(20.w),
+          child: Center(
+            child: ListView(
+
+              children: [
+              Center(
+                child: ClipOval(
+                  child: Image.network(
+                    'https://img1.baidu.com/it/u=3954207464,4167950833&fm=253&fmt=auto&app=120&f=JPEG?w=502&h=500',
+                    width: 100,
+                    height: 100,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Text('用户名'),
+              Text('ip地址'),
+
+            ],),
+          )),
     );
   }
 }
